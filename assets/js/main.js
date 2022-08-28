61336b11d58608cf182877fa2ec9234dec4f151d
kielnaingggolan07 <nainggolanhesekiel07@gmail.com>
2022-08-28 11:27:24 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    
+    $("#slider-hero").owlCarousel({
+        nav: true,
+        items: 1,
+        navtext: [
+            "<i class='fas fa-angle-left'><i>",
+            "<i class='fas fa-angle-right'><i>"
+            ],
+            navContainer: "#slider-hero-nav",
+    });
+
+    
+});
+
